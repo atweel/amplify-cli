0e58804273040fbe11278692284363f4d1a717a5
Eduard Malakhov <contactme@eduardmalakhov.com>
2020-05-26 14:01:44 +0700

    chore: add rebrand.sh

---
+++ scripts/rebrand.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+yarn clean && \
+
+lerna list --ignore @aws-amplify/cli | xargs -I {} printf "s|\\'\\\\({}\\\\)\\'|\\'@atweel/\\\\1\\'|g\ns|\\\"\\\\({}\\\\)\\\"|\\\"@atweel/\\\\1\\\"|g\n" > substitutions.txt && \
+
+printf "s|'@aws-amplify/cli'|'@atweel/amplify-cli'|g\ns|\"@aws-amplify/cli\"|\"@atweel/amplify-cli\"|g" >> substitutions.txt && \
+
+find packages -type f \( -name '*.ts' -or -name '*.js' -or -name 'package.json' \) ! -path '*/node_modules/*' -exec gsed -i -f substitutions.txt {} \; && \
+
+rm substitutions.txt && \
+
+lerna bootstrap && \
+
+yarn lint-fix && \
+
+yarn build && \
+
+yarn test
