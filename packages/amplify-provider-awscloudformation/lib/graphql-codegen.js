232c9420ab94a49c71b1f046bc3714393470cc47
Eduard Malakhov <contactme@eduardmalakhov.com>
2020-05-26 18:46:50 +0700

    chore: fix incorrect package dependency

--- packages/amplify-provider-awscloudformation/lib/graphql-codegen.js
+++ packages/amplify-provider-awscloudformation/lib/graphql-codegen.js
@@ -1,6 +1,6 @@
 let graphQLConfig;
 
-const { prePushAddGraphQLCodegenHook, prePushUpdateGraphQLCodegenHook, postPushGraphQLCodegenHook } = require('amplify-codegen');
+const { prePushAddGraphQLCodegenHook, prePushUpdateGraphQLCodegenHook, postPushGraphQLCodegenHook } = require('@atweel/amplify-codegen');
 
 async function prePushGraphQLCodegen(context, createResources, updateResources) {
   createResources = createResources.filter(resource => resource.service === 'AppSync');
